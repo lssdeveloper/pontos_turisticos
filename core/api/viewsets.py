c7c1374ebd3c62e6a5741da8e46f8d7e2bc21bd3
Leandro Serra <leandro.serra.10@gmail.com>
2021-04-13 20:36:16 -0300

    Entendendo o mecanismo de autorização

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,14 +1,19 @@
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
+
 class PontoTuristicoViewSet(ModelViewSet):
-#    queryset = PontoTuristico.objects.all() #lista todos os pontos turisticos
-    serializer_class = PontoTuristicoSerializer  #quais os campos quero mostrar
+    #    queryset = PontoTuristico.objects.all() #lista todos os pontos turisticos
+    serializer_class = PontoTuristicoSerializer  # quais os campos quero mostrar
     filter_backends = (SearchFilter,)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
     search_fields = ('nome', 'descricao', 'endereco__linha1')
     lookup_field = 'nome'
 
@@ -25,15 +30,16 @@ class PontoTuristicoViewSet(ModelViewSet):
             queryset = queryset.filter(descricao__iexact=descricao)
 
         return queryset
-    #chamando o super da classe mãe
+
+    # chamando o super da classe mãe
     def create(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
 
-#    def create(self, request, *args, **kwargs):
-#        return Response({'Helo': request.data['nome']})
+    #    def create(self, request, *args, **kwargs):
+    #        return Response({'Helo': request.data['nome']})
 
     def destroy(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
@@ -47,11 +53,10 @@ class PontoTuristicoViewSet(ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
-    @action(methods=['get'], detail=True)           #decorator
+    @action(methods=['get'], detail=True)  # decorator
     def denunciar(self, request, pk=None):
         pass
 
     @action(methods=['get'], detail=False)
     def teste(self, request):
         pass
-
